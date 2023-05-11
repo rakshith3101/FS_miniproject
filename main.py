b25fc9b0bccff8aebe2b8915a73d41a2ef792cba
rakshith3101 <rsvikas3112@gmail.com>
2023-05-11 23:17:08 +0530

    usuables created

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from usables import *
 c1='#f7ffe6'
 c2='#aaff00'
 # main window
@@ -14,14 +15,15 @@ window.resizable(width=True,height=True)
 #frame_down=Frame(window,width=500,height=500,bg='#000000')
 #frame_down.grid(row=2,column=2,padx=0,pady=1)
 
-
+frame=Frame(window,width=500,height=300,bg=c1)
+frame.pack()
 #inner frame title
-frame_label=Label(window,text="KRISHI",height=1,font=('Verdan 20 bold'),fg='#000000',bg=c2)
+frame_label=Label(frame,text="KRISHI",height=1,font=('Verdan 20 bold'),fg='#000000',bg=c2)
 frame_label.place(x=200,y=5)
 
 
 # type scheme selection
-scheme=Label(window,text='SELECT SCHEME',height=1,font=('Verdan 12 '),fg='#000000')
+scheme=Label(frame,text='SELECT SCHEME',height=1,font=('Verdan 12 '),fg='#000000')
 scheme.place(x=18,y=10)
 scheme.place(y=40)
 OPTIONS = [
@@ -32,33 +34,33 @@ OPTIONS = [
 ] 
 variable = StringVar()
 variable.set(OPTIONS[0]) # default value
-w = OptionMenu(window, variable, *OPTIONS)
+w = OptionMenu(frame, variable, *OPTIONS)
 w.pack()
 w.place(x=20,y=10)
 w.place(y=63)
-
-def ok():
-    print ("value is:" + variable.get())
-button = Button(window, text="SELECT", command=ok)
+button = Button(frame, text="SELECT", command=ok)
 button.pack()
 button.place(x=20,y=10)
 button.place(y=87)
 
 
 #regional scheme
-scheme=Label(window,text='SELECT REGION',height=1,font=('Verdan 12 '),fg='#000000')
+scheme=Label(frame,text='SELECT REGION',height=1,font=('Verdan 12 '),fg='#000000')
 scheme.place(x=375,y=10)
 scheme.place(y=40)
 
-regions=["STATE","CENTRAL"]
+regions=[
+"STATE",
+"CENTRAL"
+]
 variable1=StringVar()
 variable1.set(regions[0])
-r=OptionMenu(window,variable1,*regions)#option menu for region selection
+r=OptionMenu(frame,variable1,*regions)#option menu for region selection
 r.pack()
 r.place(x=380,y=10)
 r.place(y=63)
 
-button2=Button(window,text='SELECT',command=ok)#button for scheme
+button2=Button(frame,text='SELECT',command=example,width=5,height=1)#button for scheme
 button2.pack()
 button2.place(x=380,y=10)
 button2.place(y=87)
---
+++ usables.py
@@ -0,0 +1,14 @@
+#from main import variable
+#button for schemes 
+def ok():
+
+    pass
+    #print ("value is:" + variable.get())
+
+
+#button for region
+def example():
+    file=open('example.txt','r')
+    file_content=file.read()
+    print(file_content)
+    file.close
